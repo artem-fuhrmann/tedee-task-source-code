c971d05f0cf37d680201976440218b82b1736df4
Artem Furmanov <a.furmanov@andersenlab.com>
2023-07-29 15:09:08 +0200

    feaure: create modal feature

---
+++ src/app/modal/modal.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private _isModalOpen: boolean = false;
+
+  private _modelStatus$: BehaviorSubject<boolean> = new BehaviorSubject(
+    this._isModalOpen
+  );
+
+  getModelStatus(): Observable<boolean> {
+    return this._modelStatus$.asObservable();
+  }
+
+  public toggleModalStatus() {
+    this._isModalOpen = !this._isModalOpen;
+    this._modelStatus$.next(this._isModalOpen);
+  }
+
+  constructor() {}
+}
