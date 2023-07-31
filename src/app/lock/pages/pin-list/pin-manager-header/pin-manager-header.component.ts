482641a0f5807f89377dbea1197ab797792bb68d
Artem Furmanov <a.furmanov@andersenlab.com>
2023-07-31 12:27:29 +0200

    pin manager header ts create

---
+++ src/app/lock/pages/pin-list/pin-manager-header/pin-manager-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-manager-header',
+  templateUrl: './pin-manager-header.component.html',
+  styleUrls: ['./pin-manager-header.component.scss'],
+})
+export class PinManagerHeaderComponent {}
